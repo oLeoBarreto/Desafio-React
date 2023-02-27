f6d8958b82bef7e8f74b9c96dc54f25b4d106891
oLeoBarreto <leobcontato@gmail.com>
2023-02-27 15:31:09 -0300

    Desafio completo e funcional!

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [list, setList] = useState([]);
+  const [undid, setUndidi] = useState([]);
+
+  const handleClick = (event) => {
+    console.log("X:", event.clientX);
+    console.log("Y:", event.clientY);
+
+    const click = {
+      clientX: event.clientX,
+      clientY: event.clientY
+    }
+
+    setList((prev) => [...prev, click]);
+    setUndidi([]);
+  }
+
+  const handleUndo = (event) => {
+    event.stopPropagation();
+
+    if (list.length === 0) return;
+
+    const lastItem = list[list.length - 1];
+    setUndidi((prev) => [...prev, lastItem]);
+
+    setList((prev) => {
+      const newArray = [...prev].slice(0, -1);
+      return newArray;
+    });
+  }
+
+  const handleRedo = (event) => {
+    event.stopPropagation();
+    
+    if (undid.length === 0) return;
+
+    const recoveredItem = undid[undid.length - 1];
+    setUndidi((prev) => {
+      const newArray = [...prev].slice(0, -1);
+      return newArray;
+    });
+
+    setList((prev) => [...prev, recoveredItem]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="page" onClick={handleClick}>
+      {list.map((item, index) =>
+        <span key={index} className='circle' style={{left: item.clientX, top: item.clientY}}/>
+      )}
+      <button onClick={handleUndo}>Desfazer</button>
+      <button onClick={handleRedo}>Refazer</button>
     </div>
   );
 }
